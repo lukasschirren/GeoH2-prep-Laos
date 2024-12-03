400ed94803a297d9e0a195e5c09876a26d0d7f7b
lukasschirren <l.schirren@web.de>
2024-12-03 15:11:52 +0000

    first commit

---
+++ Inputs_Glaes/workflow_pv.py
@@ -0,0 +1,109 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Oct 23 12:29:40 2023
+
+@author: Alycia Leonard, University of Oxford
+
+workflow.py
+
+This script implements land exclusions for the countries defined in country_names
+It then allocates PV and wind installations over the allowed area
+The outputs are saved as .shp files.
+
+"""
+
+import glaes as gl
+import time
+import os
+import pickle
+
+# Define country name (used for output filenames)
+country_names = ["Laos"] 
+                # ["Benin", "Cameroon", "Congo", "Côte d'Ivoire", "Eq. Guinea", "Eritrea", "Gabon", "Gambia", "Ghana",
+                #  "Guinea", "Guinea-Bissau", "Liberia", "Libya", "Madagascar", "Mauritius", "Mozambique", "Nigeria",
+                #  "Niger", "São Tomé and Principe", "Senegal", "Seychelles", "Sierra Leone", "Somalia", "Sudan",
+                #  "Tanzania", "Togo", "Tunisia"]
+
+# Record the starting time
+start_time = time.time()
+
+# Get path to this file and then also path to data
+dirname = os.path.dirname(__file__)
+data_path = os.path.join(dirname, 'data')
+output_dir = os.path.join(dirname, 'processed')
+
+# Check whether there are directories for "data" and "processed" - make them if they're missing.
+os.makedirs(data_path, exist_ok=True)
+os.makedirs(output_dir, exist_ok=True)
+
+# create a for loop that can loop through a list of country names
+
+for country_name in country_names:
+    print("Land exclusions for " + country_name)
+
+    # Load the pickled EPSG code for the country
+    with open(os.path.join(data_path, f'{country_name}_EPSG.pkl'), 'rb') as file:
+        EPSG = pickle.load(file)
+
+    # calculating exclusions
+
+    print(" - Initializing exclusion calculator...")
+    ec = gl.ExclusionCalculator(os.path.join(data_path,  f'{country_name}.geojson'), srs=EPSG, pixelSize=100)
+    current_time = time.time() - start_time
+    print(f"   Done! Time elapsed so far: {current_time:.4f} seconds")
+
+    print(" - Applying exclusions - coast...")
+    ec.excludeVectorType(os.path.join(data_path,  f'{country_name}_oceans.geojson'), buffer=250)
+    current_time = time.time() - start_time
+    print(f"   Done! Time elapsed so far: {current_time:.4f} seconds")
+
+    # print(" - Applying exclusions - protected areas...")
+    # ec.excludeVectorType(os.path.join(data_path,  f'{country_name}_protected_areas.geojson'), buffer=250)
+    # current_time = time.time() - start_time
+    # print(f"   Done! Time elapsed so far: {current_time:.4f} seconds")
+    
+    print(" - Applying exclusions - national biodiversity conservation areas...")
+    ec.excludeVectorType(os.path.join(data_path,  f'{country_name}_national_conversation.geojson'), buffer=250)
+    current_time = time.time() - start_time
+    print(f"   Done! Time elapsed so far: {current_time:.4f} seconds")
+    
+    print(" - Applying exclusions - forest protection areas...")
+    ec.excludeVectorType(os.path.join(data_path,  f'{country_name}_protection_area.geojson'), buffer=250)
+    current_time = time.time() - start_time
+    print(f"   Done! Time elapsed so far: {current_time:.4f} seconds")
+    
+    print(" - Applying exclusions - Slope above 6.28 degrees (N/E/W) and 33 degrees (S)...")
+    ec.excludeRasterType(os.path.join(data_path, f'Laos_slope_excluded_pv.tif'), value=1, prewarp=True)
+    current_time = time.time() - start_time
+    print(f"   Done! Time elapsed so far: {current_time:.4f} seconds")
+
+    print(" - Applying exclusions - herbaceous wetland...")
+    ec.excludeRasterType(os.path.join(data_path, f'{country_name}_CLC.tif'), value=90, prewarp=True)
+    current_time = time.time() - start_time
+    print(f"   Done! Time elapsed so far: {current_time:.4f} seconds")
+
+    print(" - Applying exclusions - built-up area...")
+    ec.excludeRasterType(os.path.join(data_path, f'{country_name}_CLC.tif'), value=50, prewarp=True)
+    current_time = time.time() - start_time
+    print(f"   Done! Time elapsed so far: {current_time:.4f} seconds")
+
+    print(" - Applying exclusions - permanent water bodies...")
+    ec.excludeRasterType(os.path.join(data_path, f'{country_name}_CLC.tif'), value=80, prewarp=True)
+    current_time = time.time() - start_time
+    print(f"   Done! Time elapsed so far: {current_time:.4f} seconds")
+    ec.draw()
+
+    print(" - Applying exclusions - agriculture...")
+    ec.excludeRasterType(os.path.join(data_path, f'{country_name}_CLC.tif'), value=40, prewarp=True)
+    current_time = time.time() - start_time
+    print(f"   Done! Time elapsed so far: {current_time:.4f} seconds")
+
+    print(" - Saving excluded areas for PV as .tif file...")
+    ec.save(os.path.join(output_dir, f'{country_name}_pv_exclusions.tif'), overwrite=True)
+    current_time = time.time() - start_time
+    print(f"   Done! Time elapsed so far: {current_time:.4f} seconds")
+
+    print(" - Distributing pv plants and saving placements as .shp...")
+    ec.distributeItems(separation=224, output=os.path.join(output_dir, f'{country_name}_pv_placements.shp'))
+    current_time = time.time() - start_time
+    print(f"   Done! Time elapsed so far: {current_time:.4f} seconds")
---
+++ combine_glaes_spider.py
@@ -0,0 +1,80 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Oct 19 11:21:15 2023
+
+@author: Alycia Leonard, University of Oxford
+
+spatial_data_prep.py
+
+This script joins the outputs from GLAES to the hexagons produced by SPIDER for input to GEOH2.
+The inputs are the SPIDER hex.geojson file and the GLAES pv_placements.shp and turbine_placements.shp files
+The output is a hexagon file where a count of turbine and pv installations is attached to the hexagons saved in Data.
+
+"""
+
+import geopandas as gpd
+import os
+from unidecode import unidecode
+
+# Define country name (used for output filenames)
+#"Benin", "Cameroon", "Congo", "Côte d'Ivoire", "Eq. Guinea", "Eritrea", "Gabon", "Gambia", "Ghana", "Guinea", "Guinea-Bissau", "Liberia", "Libya", "Madagascar", "Mauritius", "Mozambique", "Nigeria", "Niger",
+#country_names = ["Algeria", "Angola", "Dem. Rep. Congo", "Cabo Verde", "Djibouti", "Egypt", "Kenya", "Mauritania", "Morocco", "Namibia", "South Africa"]
+#country_names = ["São Tomé and Principe", "Senegal", "Seychelles", "Sierra Leone", "Somalia", "Sudan", "Tanzania", "Togo", "Tunisia"]
+country_names = ["Laos"]
+
+# Get path to this file
+dirname = os.path.dirname(__file__)
+
+# create a for loop that can loop through a list of country names
+for country_name in country_names:
+
+    print(f"Combining GLAES and SPIDER data for {country_name}!")
+
+    # Get country names without accents, spaces, apostrophes, or periods for loading files
+    country_name_clean = unidecode(country_name)
+    country_name_clean = country_name_clean.replace(" ", "")
+    country_name_clean = country_name_clean.replace(".", "")
+    country_name_clean = country_name_clean.replace("'", "")
+
+    # Get paths
+    hex_path = os.path.join(dirname, "Inputs_Spider", "processed", f"{country_name_clean}_hex.geojson")
+    wind_path = os.path.join(dirname, "Inputs_Glaes", "processed", f"{country_name}_turbine_placements.shp")
+    pv_path = os.path.join(dirname, "Inputs_Glaes", "processed", f"{country_name}_pv_placements.shp")
+    save_path = os.path.join(dirname, "Inputs_GeoH2", "Data", f"{country_name_clean}_hex_final.geojson")
+
+    # Load all files and convert all to the country's CRS
+    print(" - Loading files...")
+    hex = gpd.read_file(hex_path)
+    wind_points = gpd.read_file(wind_path)
+    pv_points = gpd.read_file(pv_path)
+    hex.to_crs(pv_points.crs, inplace=True)
+
+    print(" - Joining turbine locations...")
+    # Spatial join the wind points to the polygons
+    spatial_join = gpd.sjoin(wind_points, hex, how='left', predicate='within')
+
+    # Group by polygon and count the points within each polygon
+    wind_point_counts = spatial_join.groupby('index_right0').size()
+
+    # Merge the point counts with the 'hex' GeoDataFrame based on the index
+    hex['theo_turbines'] = wind_point_counts
+
+    # If some polygons have no points, fill their 'point_count' with 0
+    hex['theo_turbines'].fillna(0, inplace=True)
+
+    print(" - Joining pv locations...")
+    # Spatial join the pv points to the polygons
+    spatial_join = gpd.sjoin(pv_points, hex, how='left', predicate='within')
+
+    # Group by polygon and count the points within each polygon
+    pv_point_counts = spatial_join.groupby('index_right0').size()
+
+    # Merge the point counts with the 'hex' GeoDataFrame based on the index
+    hex['theo_pv'] = pv_point_counts
+
+    # If some polygons have no points, fill their 'point_count' with 0
+    hex['theo_pv'].fillna(0, inplace=True)
+
+    print(" - Done! Saving to GeoJSON.")
+    # Save the file
+    hex.to_file(save_path, driver='GeoJSON', encoding='utf-8')
